7bd3678e03009bb20c831a0adac84b9820a194ff
Zino Ebiala <eebia002@fiu.edu>
2023-08-17 21:09:35 -0400

    added webpack plugins

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -21,5 +21,5 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    
+    window.defferedPrompt = null;
 });
